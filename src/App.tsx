5a8e9ef6108dc8509c42690988e8ed9b01682307
Alaz Hamşioğlu <73095897+AlazHamsioglu@users.noreply.github.com>
2026-08-11 11:32:26 +0300

    chore: initialize React TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Mama Takip</h1>
+      <p>Evcil hayvanlarının günlük beslenmesini takip et.</p>
+    </main>
+  );
+}
+
+export default App;
